fa54c68817f116e5a829133156b07f03451238a8
eWeidling <erik.weidling@gmx.de>
2023-05-08 17:40:56 +0200

    SwordOnUse
    SwordOnUse Methode getEntitiesToHit funktionienert, aber hitInDirection noch nicht

--- game/src/ecs/items/IOnUseBehavior/SwordOnUse.java
+++ game/src/ecs/items/IOnUseBehavior/SwordOnUse.java
@@ -1,7 +1,5 @@
 package ecs.items.IOnUseBehavior;
 
-import com.badlogic.gdx.ai.pfa.Connection;
-import com.badlogic.gdx.utils.Array;
 import ecs.components.PositionComponent;
 import ecs.entities.Entity;
 import ecs.entities.Faction;
@@ -9,8 +7,6 @@ import ecs.entities.Hero;
 import ecs.items.IOnUse;
 import ecs.items.ItemData;
 import ecs.systems.PlayerSystem;
-import level.elements.tile.FloorTile;
-import level.elements.tile.Tile;
 import starter.Game;
 import tools.Point;
 
@@ -25,32 +21,64 @@ public class SwordOnUse implements IOnUse {
     @Override
     public void onUse(Entity e, ItemData item) {
         this.hero = (Hero) e;
+        ArrayList<Entity> entities = getEntitiesToHit();
     }
 
-    public ArrayList<Entity> getFloorTilesToHit() {
+    private ArrayList<Entity> getEntitiesToHit() {
         Set<Entity> entities = Game.getEntities();
         ArrayList<Entity> ents = new ArrayList<>();
+        ArrayList<Entity> entsToHit = new ArrayList<>();
         for (Entity ente : entities) {
             if (ente.getFaction() == Faction.FOE)
                 ents.add(ente);
         }
-        Hero hero = (Hero) Game.getHero().get();
-        PositionComponent pos = (PositionComponent) hero.getComponent(PositionComponent.class).get();
-        Point heroPoint = pos.getPosition();
         PlayerSystem lastKeyStroke = new PlayerSystem();
         // Hit nach links
         if (lastKeyStroke.getKey() == 0) {
             System.out.println("hit left funzt");
+             entsToHit = hitInDirection(0,ents);
             // Hit nach oben
         } else if (lastKeyStroke.getKey() == 1) {
             System.out.println("hit oben funzt");
+            entsToHit = hitInDirection(1,ents);
             // Hit nach rechts
         } else if (lastKeyStroke.getKey() == 2) {
             System.out.println("hit rechts funzt");
+            entsToHit = hitInDirection(2,ents);
             // Hit nach unten
         } else if (lastKeyStroke.getKey() == 3) {
             System.out.println("hit unten funzt");
+            entsToHit = hitInDirection(3,ents);
         }
         return ents;
+    }
+    private ArrayList<Entity> hitInDirection(int key, ArrayList<Entity> entities) {
+        Hero hero = (Hero) Game.getHero().get();
+        PositionComponent pos = (PositionComponent) hero.getComponent(PositionComponent.class).get();
+        Point heroPoint = pos.getPosition();
+        ArrayList<Entity> entsToHit = new ArrayList<>();
+        for ( Entity ente : entities) {
+            PositionComponent posOfEnte = (PositionComponent) ente.getComponent(PositionComponent.class).get();
+            Point pointOfEnte = posOfEnte.getPosition();
+            boolean checkRight = heroPoint.x+1 == pointOfEnte.x;
+            boolean checkLeft = heroPoint.x-1 == pointOfEnte.x;
+            boolean checkUpper = heroPoint.y-1 == pointOfEnte.y;
+            boolean checkDown = heroPoint.y+1 == pointOfEnte.y;
+            boolean checkUpperRight = (checkRight && checkUpper);
+            boolean checkUpperLeft = (checkLeft && checkUpper);
+            boolean checkDownRight = (checkRight && checkDown);
+            boolean checkDownLeft = (checkLeft && checkDown);
+             if ( key == 0 && ( checkLeft || checkUpperLeft || checkDownLeft)) {
+                 entsToHit.add(ente);
+                 System.out.println("hit left funzt2");
+             }
+              else if ( key == 1 && ( checkUpper || checkUpperLeft || checkUpperRight ))
+                 entsToHit.add(ente);
+              else if ( key == 2 && ( checkRight || checkUpperRight || checkDownRight))
+                  entsToHit.add(ente);
+              else if ( key == 3 && ( checkDown || checkDownLeft || checkDownRight))
+                  entsToHit.add(ente);
+        }
+        return entsToHit;
     }
 }
