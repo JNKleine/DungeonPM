1a579580efed73230ddf7eba42ca8286534876f6
eWeidling <erik.weidling@gmx.de>
2023-05-10 12:37:08 +0200

    Random namen für den Ghost

--- game/src/ecs/entities/Ghost.java
+++ game/src/ecs/entities/Ghost.java
@@ -21,66 +21,75 @@ import java.util.Random;
  */
 public class Ghost extends Monster {
 
-    public static String name = "Gilbert";
+    public static String name;
 
-    /**Creates an object from type ghost
+    /**
+     * Creates an object from type ghost
      *
-     *<p>Within this constructor, all defined and required values
+     * <p>Within this constructor, all defined and required values
      * are passed to the Monster superclass.
      * In addition, Ghost receives specific components here that were not
      * generally received in monsters.</p>
-     * */
+     */
     public Ghost() {
         super(0.1f, 0.1f, 0, 0, 0.9f, 1, "npc/ghost/idleRight",
             "npc/ghost/idleLeft", "npc/ghost/runRight", "npc/ghost/runLeft", Faction.NEUTRAL);
         addHitBox();
         addInteractionComponent();
         Random random = new Random();
-        if(random.nextInt(0,2) == 0 || Game.currentLevelNumber == 10) {
+        if (random.nextInt(0, 2) == 0 || Game.currentLevelNumber == 10) {
             addAIComponent();
         } else {
             alternativeAIComponent();
         }
+        this.name = getRandomName();
     }
 
     private void addInteractionComponent() {
-        new InteractionComponent(this,1f,true,new OpenDialogueOnInteraction());
+        new InteractionComponent(this, 1f, true, new OpenDialogueOnInteraction());
     }
 
     //add HitBoxComponent
     private void addHitBox() {
         new HitboxComponent(
             this,
-            (you,other,direction) -> System.out.println("GhostCollisionEnter"),
-            (you,other,direction) -> System.out.println("GhostCollisionLeave"));
+            (you, other, direction) -> System.out.println("GhostCollisionEnter"),
+            (you, other, direction) -> System.out.println("GhostCollisionLeave"));
     }
+
     //add AIComponent
     private void addAIComponent() {
         CollideAI fightAI = new CollideAI(3f);
         FollowWalk idleAI = new FollowWalk();
         RangeTransition transitionAI = new RangeTransition(1f);
 
-        new AIComponent(this, fightAI,idleAI,transitionAI);
+        new AIComponent(this, fightAI, idleAI, transitionAI);
 
     }
 
     //add alternative AIComponent
     private void alternativeAIComponent() {
         CollideAI fightAI = new CollideAI(3f);
-        RadiusWalk idleAI = new RadiusWalk(10,2);
+        RadiusWalk idleAI = new RadiusWalk(10, 2);
         RangeTransition transitionAI = new RangeTransition(1f);
 
-        new AIComponent(this, fightAI,idleAI,transitionAI);
+        new AIComponent(this, fightAI, idleAI, transitionAI);
     }
 
     /**
      * Determine what should happen when this entity collides with another
      *
      * @param hb HitBoxComponent from other entity
-     * */
+     */
     @Override
-    public void onHit( HitboxComponent hb) {
-        }
+    public void onHit(HitboxComponent hb) {
+    }
+
+    private String getRandomName() {
+        String[] randomNames = {"Gilbert, Hans, Peter, Joe, Sabine, Lena, Paul, Erik, Frank"};
+        Random rdm = new Random();
+        return randomNames[rdm.nextInt(randomNames.length)];
+    }
 }
 
 
