41ce828b9478ea5061848e735a65c23c7736c9de
eWeidling <erik.weidling@gmx.de>
2023-06-06 22:26:58 +0200

    bugfix boss

--- game/src/starter/Game.java
+++ game/src/starter/Game.java
@@ -547,6 +547,7 @@ public class Game extends ScreenAdapter implements IOnLevelLoader {
         getHero().get().removeComponent(InventoryComponent.class);
         ((Hero)hero).setupInventoryComponent();
         restarted = true;
+        bossRoom = false;
         getHero().get().removeComponent(QuestLogComponent.class);
         hero.addComponent(new QuestLogComponent(hero,10));
         Ghost.questIsAccepted = false;
